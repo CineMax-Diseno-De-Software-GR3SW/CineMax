0f01b03198f7f2ccca2b3f0b6c22df04c4c42919
SebastianSarasti <sebastiansarasti.14@gmail.com>
2025-07-10 11:11:16 -0500

    Pantallas_Cartelera_Funciones

--- src/main/java/com/cinemax/moduloboletos/Servicios/ServicioCineMax.java
+++ src/main/java/com/cinemax/moduloboletos/Servicios/ServicioCineMax.java
@@ -14,36 +14,37 @@ public class ServicioCineMax {
         controladorVentaDeBoletos = new ControladorVentaDeBoletos();
     }
 
-    public int manejarInicio(CineMax cineMax, VistaCineMax vistaPaginaPrincipal, ControladorCineMax controladorCineMax) {
+    public int manejarInicio(CineMax cineMax, VistaCineMax vistaPaginaPrincipal,
+            ControladorCineMax controladorCineMax) {
         List<String> opciones = cineMax.obtenerOpciones();
         int cerrado = 0;
-        do{
-            cerrado =vistaPaginaPrincipal.mostrar(opciones);
+        do {
+            cerrado = vistaPaginaPrincipal.mostrar(opciones);
             switch (cerrado) {
                 case 1:
                     // Aquí va la gestión de películas...
                     break;
                 case 2:
-                    //Aquí va la gestión de salas...
+                    // Aquí va la gestión de salas...
                     break;
                 case 3:
-                    //Aquí va la gestión de boletos...
+                    // Aquí va la gestión de boletos...
 
-//                    controladorVentaDeBoletos.gestionarVentaDeBoletos("Controlador de Cartelera", "Controlador de Consulta de Salas");
+                    // controladorVentaDeBoletos.gestionarVentaDeBoletos("Controlador de Cartelera",
+                    // "Controlador de Consulta de Salas");
                     controladorCineMax.iniciarModuloVentaBoletos();
                     break;
                 case 4:
-                    //Aquí va la gestión de empleados...
+                    // Aquí va la gestión de empleados...
                     break;
                 case 5:
-                    //Aquí va la gestión de reportes...
+                    // Aquí va la gestión de reportes...
                     break;
                 default:
                     break;
             }
-        }while(cerrado != 6);
+        } while (cerrado != 6);
         return cerrado;
     }
 
-    
 }
