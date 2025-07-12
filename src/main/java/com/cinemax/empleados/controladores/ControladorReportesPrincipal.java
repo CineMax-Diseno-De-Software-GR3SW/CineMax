6f510457abe58765647c2e8205b394ab3bdf7f7a
Alexis Sotomayor <89486598+Alexiskeepcoding@users.noreply.github.com>
2025-07-12 16:55:36 -0500

    feat: Add controller for the main report screen and a draft of the principal template

---
+++ src/main/java/com/cinemax/empleados/controladores/ControladorReportesPrincipal.java
@@ -0,0 +1,49 @@
+package com.cinemax.empleados.controladores;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ControladorReportesPrincipal {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void onBackAction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/empleados/PantallaPortalPrincipal.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void onCerrarSesion(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/empleados/PantallaLogin.fxml"));
+        try {
+            Parent root = loader.load();
+
+            // Obtener el Stage actual desde el botón o cualquier nodo
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setTitle("Portal del Administrador");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
