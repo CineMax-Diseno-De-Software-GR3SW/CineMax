1dea473c2e76ed130d4a47cc6e45e4fe3dcd0ec7
DorianAlban <dorian.alban@epn.edu.ec>
2025-07-23 01:40:31 -0500

    Modificacion a SalasVIPFactory

--- src/main/java/com/cinemax/salas/modelos/entidades/SalaVIPFactory.java
+++ src/main/java/com/cinemax/salas/modelos/entidades/SalaVIPFactory.java
@@ -1,4 +1,8 @@
 package com.cinemax.salas.modelos.entidades;
 
-public class SalaVIPFactory {
+public class SalaVIPFactory extends SalaFactory {
+    @Override
+    public Sala crearSala(int id, String nombre, int capacidad, EstadoSala estado) {
+        return new Sala(id, nombre, capacidad, TipoSala.VIP, estado);
+    }
 }
