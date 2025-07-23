d814ec11145b3f280e2ac1010eb9bc1b39d1cd6f
Claudio Peñaherrera <claudioomar2010abc@gmail.com>
2025-07-23 01:37:12 -0500

    Modificacion Sala Normal Factory

--- src/main/java/com/cinemax/salas/modelos/entidades/SalaNormalFactory.java
+++ src/main/java/com/cinemax/salas/modelos/entidades/SalaNormalFactory.java
@@ -1,4 +1,8 @@
 package com.cinemax.salas.modelos.entidades;
 
-public class SalaNormalFactory {
+public class SalaNormalFactory extends SalaFactory {
+    @Override
+    public Sala crearSala(int id, String nombre, int capacidad, EstadoSala estado) {
+        return new Sala(id, nombre, capacidad, TipoSala.NORMAL, estado);
+    }
 }
