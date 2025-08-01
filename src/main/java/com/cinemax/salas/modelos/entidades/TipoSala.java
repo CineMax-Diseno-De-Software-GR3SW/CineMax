6aecc809b77475695a24807b6aa512d339144a48
SaulT-G <saul.tualombo@epn.edu.ec>
2025-07-31 21:59:09 -0500

    Añadir multiplicador

--- src/main/java/com/cinemax/salas/modelos/entidades/TipoSala.java
+++ src/main/java/com/cinemax/salas/modelos/entidades/TipoSala.java
@@ -1,6 +1,15 @@
 package com.cinemax.salas.modelos.entidades;
 
 public enum TipoSala {
-    NORMAL,
-    VIP
+    NORMAL(1),
+    VIP(2),;
+
+    private static int multiplicador;
+
+    TipoSala(int multiplicador) {
+        this.multiplicador = multiplicador;
+    }
+    public int getMultiplicador() {
+        return multiplicador;
+    }
 }
